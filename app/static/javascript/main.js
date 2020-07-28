d8dccf6b43862458417e7e353d4ef0685c4edb5e
Cedric Murairi <murairicedric@gmail.com>
2020-07-28 22:15:06 +0200

    refactoring js file

--- app/static/javascript/main.js
+++ app/static/javascript/main.js
@@ -3,33 +3,29 @@ var exit_button_all = document.querySelectorAll('.exit_button');
 
 var source = Handlebars.compile(document.getElementById("entry-template").innerHTML);
 
+// compile the visitor card template blueprint for futute usage
 var template_card_blueprint = Handlebars.compile(document.getElementById("template_card").innerHTML);
 
-var html = source({ title: "My New Post", body: "This is my first post!" });
+// var html = source({ title: "My New Post", body: "This is my first post!" });
 
-console.log(html);
-
-// var visitor_result = [{'name': 'cedric', 'email': 'c.murairi@gmail.com', 'id': 1},{'name': 'Sylvain', 'email': 's.bebutsa@gmail.com', 'id': 2}];
-
-// var final_card_template = template_card_blueprint({ visitor: visitor_result });
-
-// console.log(final_card_template);
-// test block, test code end of
-// document.getElementById('main_section').innerHTML = final_card_template;
-// console.log(content1);
+// console.log(html);
 
 window.addEventListener('click', event => {
+	// check if the user clicks the cover abckground of the show-detail card to hide both the card and the cover
 	if (event.target === document.querySelector('.cover')) {
 		document.querySelector('.cover').style.display = "none";
 		document.querySelectorAll('.show_details').forEach(card => {
 			card.style.display = "none";
 		})
 	}
+
 });
 
 
 if(document.querySelector('#all_search') !== null){
+
 	document.querySelector('#all_search').onsubmit = () => {
+
 		let visitor_result = [];
 		alert('submitted');
 		console.log('submitted');
@@ -38,18 +34,27 @@ if(document.querySelector('#all_search') !== null){
 		var request = new XMLHttpRequest();
 
 		request.onload = function () {
+
 			if (request.readyState === XMLHttpRequest.DONE) {
+
 				if (request.status === 200) {
 					// document.querySelector('#entries').innerHTML = JSON.parse(request.responseText);
 					let response = JSON.parse(request.responseText);
-					console.log(response);
+					// console.log(response);
 					try{
+						// loop over the array json returned by the server and check for the boolean value *visiot_exist
 						response.forEach((element_response) => {
+
 							if(element_response.visitor_exist){
 								visitor_result.push(element_response);
 							}
+
 						});
-						console.log(visitor_result);
+						// console.log(visitor_result);
+						let final_card_template = template_card_blueprint({ visitor: visitor_result });
+						// console.log(final_card_template);
+						document.getElementById('cards_container').innerHTML = final_card_template;
+
 					}catch(e){
 						console.log({'message': 'No such a visitor in our records'});
 						console.log(e);
@@ -72,52 +77,60 @@ if(document.querySelector('#all_search') !== null){
 }
 
 
-
-document.querySelectorAll('.visitor_detail_button').forEach(function (button) {
-	button.addEventListener('click', function () {
-		var id = button.dataset.id;
-		document.querySelectorAll('.show_details').forEach(function (card_detail) {
-			if (card_detail.dataset.id === id) {
-				card_detail.style.display = "block";
-				document.querySelector('.cover').style.display = "block";
-			}
-		});
+document.querySelectorAll('.visitor_detail_button').forEach((button) => {
+	button.addEventListener('click', () => {
+		showCardDetail(button);
 	})
 });
 
+
+function showCardDetail(button){
+	var id = button.dataset.id;
+	document.querySelectorAll('.show_details').forEach(function (card_detail) {
+		// check if the visitor card data-id is equal to the clicked button's id to display details about that specif user id
+		if (card_detail.dataset.id === id) {
+			card_detail.style.display = "block";
+			document.querySelector('.cover').style.display = "block";
+		}
+
+	});
+}
+
+
 exit_button_all.forEach(function (button) {
 	button.addEventListener('click', function () {
+		// visitor id is the id of the clicked button
 		var visitor_id = this.dataset.id;
 		// console.log(visitor_id);
 
 		var httpRequest = new XMLHttpRequest();
 
-		// request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		// httpRequest.open("POST", '/exit', true);
-
 		httpRequest.onload = function () {
 			try {
+
 				if (httpRequest.readyState === XMLHttpRequest.DONE) {
+
 					if (httpRequest.status === 200) {
 						console.log(JSON.parse(httpRequest.responseText));
 						document.querySelectorAll('.card').forEach(function (card) {
 							var id = card.dataset.id;
+							// check if the data-id of the card holding the button is the same as the button's data-id -> visitor_id to change it's animation to running and remove the card
 							if (id === visitor_id) {
 								card.style.animationPlayState = "running";
 							}
+
 						});
 
-						// history.pushState(null, 'exit', 'exit')
 					}
+
 					else
 						console.log("nothing here little boy");
 				}
+
 			} catch (error) {
 				console.log(error);
 			}
 		};
-
-		// httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 		var data = new FormData();
 		data.append('id', visitor_id);
