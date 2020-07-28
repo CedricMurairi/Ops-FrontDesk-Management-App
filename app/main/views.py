e30ae361c195892752e2e96f4f3f4ed548de5915
Cedric Murairi <murairicedric@gmail.com>
2020-07-28 09:45:42 +0200

    add and refactor view functions to return arrays in json

--- app/main/views.py
+++ app/main/views.py
@@ -8,11 +8,13 @@ from app import db
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
+
 	return render_template('index.html')
 
 
 @main.route('/entrance', methods=['GET', 'POST'])
 def entrance():
+
 	name = request.form.get('name')
 	email = request.form.get('email')
 	person_visited = request.form.get('person_visited')
@@ -31,7 +33,9 @@ def entrance():
 
 @main.route('/entries/actives', methods=['GET', 'POST'])
 def actives_entries():
+
 	actives_entries = Visitor.query.filter_by(is_out=False).all()
+
 	for visitor in actives_entries:
 		print(visitor.name)
 
@@ -40,6 +44,7 @@ def actives_entries():
 
 @main.route('/entries/all', methods=['GET', 'POST'])
 def all_entries():
+
 	if request.method == 'GET':
 		all_entries = Visitor.query.all()
 		for visitor in all_entries:
@@ -50,10 +55,13 @@ def all_entries():
 
 @main.route('/exit', methods=['POST'])
 def exit():
+
 	visitor_id = request.form.get('id')
 	visitor = Visitor.query.filter_by(id=visitor_id).first()
+
 	if(visitor.is_out):
 		return jsonify({'visitor_already_out': True})
+
 	visitor.is_out = True
 	visitor.exit_time = datetime.utcnow()
 	db.session.add(visitor)
@@ -63,9 +71,15 @@ def exit():
 
 @main.route('/search', methods=['POST'])
 def search():
+
 	visitor_name = request.form.get('visitor_name')
-	visitor = Visitor.query.filter_by(name=visitor_name).first()
-	if visitor is None:
+	visitors = Visitor.query.filter_by(name=visitor_name).all()
+
+	if not visitors:
 		return jsonify({"visitor_exist": False, "message": "No such a visitor in the databse"})
-	print(visitor)
-	return jsonify({"visitor_exist": True, "visitor_id": visitor.id, "visitor_name": visitor.name, "visitor_email": visitor.email, "visitor_person_visted": visitor.person_visited, "visitor_entry_time": visitor.entry_time, "visitor_exit_time": visitor.exit_time, "visitor_is_out": visitor.is_out, })
+
+	print(visitors)
+	for visitor in visitors:
+		visitors_list = [{"visitor_exist": True, "visitor_id": visitor.id, "visitor_name": visitor.name, "visitor_email": visitor.email, "visitor_person_visted": visitor.person_visited, "visitor_entry_time": visitor.entry_time, "visitor_exit_time": visitor.exit_time, "visitor_is_out": visitor.is_out, }]
+
+	return jsonify(visitors_list)
